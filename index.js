dcb1f2d028a97cbec0e81605538190d40193727d
Amin Hasan <amin@Amins-MacBook-Air.local>
2020-03-10 10:24:16 +0000

    added get and post requests

--- index.js
+++ index.js
@@ -8,9 +8,35 @@ const app = express()
 // condifgure app so responses
 // carry special headers to enable CORS
 app.use(cors())
+// by default express cannot parse bodies of requests
+// we need to opt-in to body-parsing
+// and this comes with express
+app.use(express.json())
+
+const friends = ['Alison', 'Zak', 'Rufai']
+
+app.get('/api/friends', (req, res) => {
+
+    // res.json()
+    // res.send()
+    // res.end()
+
+    res.status(200).json(friends)
+})
+
+// make me a POST that has the desired name somewhere in the request
+// send back???
+
+app.post('/api/friends', (req, res) => {
+    console.log(req.body)
+    const { friend } = req.body
+    friends.push(friend)
+    res.status(201).json(friend)
+})
 
 const PORT = 3000
 
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}`);
 })
+
